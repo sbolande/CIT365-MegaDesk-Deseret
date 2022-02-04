ca39717a0594d36d711d7d57167dbff8b99fc6f1
dirogers <rog17006@byui.edu>
2022-02-04 14:04:48 -0700

    Adding View all but has some issues

--- MegaDesk-Deseret/ViewAllQuotes.Designer.cs
+++ MegaDesk-Deseret/ViewAllQuotes.Designer.cs
@@ -28,35 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.jsonDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jsonDataBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // menuBtn
             // 
             this.menuBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.menuBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.menuBtn.Location = new System.Drawing.Point(608, 400);
+            this.menuBtn.Location = new System.Drawing.Point(937, 373);
+            this.menuBtn.Margin = new System.Windows.Forms.Padding(2);
             this.menuBtn.Name = "menuBtn";
-            this.menuBtn.Size = new System.Drawing.Size(180, 38);
+            this.menuBtn.Size = new System.Drawing.Size(135, 31);
             this.menuBtn.TabIndex = 4;
             this.menuBtn.Text = "Go &Back";
             this.menuBtn.UseVisualStyleBackColor = true;
             this.menuBtn.Click += new System.EventHandler(this.backBtn_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1059, 343);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(478, 360);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // jsonDataBindingSource
+            // 
+            this.jsonDataBindingSource.DataSource = typeof(MegaDesk_Deseret.JsonData);
+            // 
             // ViewAllQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1083, 415);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ViewAllQuotes";
             this.Text = "ViewAllQuotes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jsonDataBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button menuBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource jsonDataBindingSource;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- MegaDesk-Deseret/ViewAllQuotes.cs
+++ MegaDesk-Deseret/ViewAllQuotes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace MegaDesk_Deseret
 {
@@ -14,10 +15,16 @@ namespace MegaDesk_Deseret
     {
         readonly List<DeskQuote> _quotes = JsonData.Read();
 
+        private const string _filepath = @"./quotes.json";
+
+        
+
         public ViewAllQuotes()
         {
             InitializeComponent();
+            DisplayQuotes();
         }
+
 
         /// <summary>
         /// Go back to main menu. Triggers on Back Button click.
@@ -27,6 +34,39 @@ namespace MegaDesk_Deseret
             var mainMenu = (MainMenu)Tag;
             mainMenu.Show();
             Close();
+        }
+
+        private void DisplayQuotes()
+        {
+
+            List<DeskQuote> quotes = JsonData.Read();
+            Console.WriteLine(quotes);
+
+
+            foreach (DeskQuote quote in quotes)
+            {
+                string[] row = {quote.FirstName, quote.LastName, quote.Desk.DesktopMaterial.ToString(), quote.Desk.Width.ToString(), quote.Desk.Depth.ToString(),
+                quote.Desk.DrawerCount.ToString(), quote.CalculateQuote().ToString(), quote.Desk.ProductionDays.ToString() };
+                dataGridView1.Rows.Add(row);
+
+                //This is just to make sure stuff is getting passed to the small text box. (just a check)
+                textBox1.Text = row[7];
+         
+                
+                
+            }
+
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
