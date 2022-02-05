23e171ef3ed6425d16f4a9c25109b37123f38939
Seth Bolander <bol15015@byui.edu>
2022-02-04 18:25:50 -0700

    Fixed title

--- MegaDesk-Deseret/MainMenu.Designer.cs
+++ MegaDesk-Deseret/MainMenu.Designer.cs
@@ -41,10 +41,10 @@
             // 
             this.addQuoteBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.addQuoteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.addQuoteBtn.Location = new System.Drawing.Point(22, 35);
-            this.addQuoteBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addQuoteBtn.Location = new System.Drawing.Point(29, 43);
+            this.addQuoteBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addQuoteBtn.Name = "addQuoteBtn";
-            this.addQuoteBtn.Size = new System.Drawing.Size(172, 58);
+            this.addQuoteBtn.Size = new System.Drawing.Size(229, 71);
             this.addQuoteBtn.TabIndex = 1;
             this.addQuoteBtn.Text = "&Add New Quote";
             this.addQuoteBtn.UseVisualStyleBackColor = true;
@@ -54,10 +54,10 @@
             // 
             this.viewQuotesBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.viewQuotesBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.viewQuotesBtn.Location = new System.Drawing.Point(22, 105);
-            this.viewQuotesBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.viewQuotesBtn.Location = new System.Drawing.Point(29, 129);
+            this.viewQuotesBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.viewQuotesBtn.Name = "viewQuotesBtn";
-            this.viewQuotesBtn.Size = new System.Drawing.Size(172, 58);
+            this.viewQuotesBtn.Size = new System.Drawing.Size(229, 71);
             this.viewQuotesBtn.TabIndex = 2;
             this.viewQuotesBtn.Text = "&View Quotes";
             this.viewQuotesBtn.UseVisualStyleBackColor = true;
@@ -67,10 +67,10 @@
             // 
             this.searchQuotesBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.searchQuotesBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.searchQuotesBtn.Location = new System.Drawing.Point(22, 175);
-            this.searchQuotesBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.searchQuotesBtn.Location = new System.Drawing.Point(29, 215);
+            this.searchQuotesBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.searchQuotesBtn.Name = "searchQuotesBtn";
-            this.searchQuotesBtn.Size = new System.Drawing.Size(172, 58);
+            this.searchQuotesBtn.Size = new System.Drawing.Size(229, 71);
             this.searchQuotesBtn.TabIndex = 3;
             this.searchQuotesBtn.Text = "&Search Quotes";
             this.searchQuotesBtn.UseVisualStyleBackColor = true;
@@ -80,10 +80,10 @@
             // 
             this.exitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.exitBtn.Location = new System.Drawing.Point(22, 245);
-            this.exitBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.exitBtn.Location = new System.Drawing.Point(29, 302);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.exitBtn.Name = "exitBtn";
-            this.exitBtn.Size = new System.Drawing.Size(172, 58);
+            this.exitBtn.Size = new System.Drawing.Size(229, 71);
             this.exitBtn.TabIndex = 4;
             this.exitBtn.Text = "E&xit";
             this.exitBtn.UseVisualStyleBackColor = true;
@@ -94,26 +94,27 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(266, 35);
+            this.pictureBox1.Location = new System.Drawing.Point(355, 43);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(268, 268);
+            this.pictureBox1.Size = new System.Drawing.Size(357, 330);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.exitBtn);
             this.Controls.Add(this.searchQuotesBtn);
             this.Controls.Add(this.viewQuotesBtn);
             this.Controls.Add(this.addQuoteBtn);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainMenu";
-            this.Text = "MegaDesk-Bolander";
+            this.Text = "MegaDesk-Deseret";
             this.Load += new System.EventHandler(this.MainMenu_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
