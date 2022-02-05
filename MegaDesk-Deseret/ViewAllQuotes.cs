88550c54fd23faa9ba7198907d69fffd034574cf
Seth Bolander <bol15015@byui.edu>
2022-02-04 18:11:03 -0700

    Updated DisplayQuotes()

--- MegaDesk-Deseret/ViewAllQuotes.cs
+++ MegaDesk-Deseret/ViewAllQuotes.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json;
 
 namespace MegaDesk_Deseret
 {
@@ -15,16 +14,12 @@ namespace MegaDesk_Deseret
     {
         readonly List<DeskQuote> _quotes = JsonData.Read();
 
-        private const string _filepath = @"./quotes.json";
-
-        
-
         public ViewAllQuotes()
         {
             InitializeComponent();
+            // TODO: Add all 8 columns to dataGridView1
             DisplayQuotes();
         }
-
 
         /// <summary>
         /// Go back to main menu. Triggers on Back Button click.
@@ -38,25 +33,20 @@ namespace MegaDesk_Deseret
 
         private void DisplayQuotes()
         {
-
-            List<DeskQuote> quotes = JsonData.Read();
-            Console.WriteLine(quotes);
-
-
-            foreach (DeskQuote quote in quotes)
+            _quotes.ForEach(quote =>
             {
-                string[] row = {quote.FirstName, quote.LastName, quote.Desk.DesktopMaterial.ToString(), quote.Desk.Width.ToString(), quote.Desk.Depth.ToString(),
-                quote.Desk.DrawerCount.ToString(), quote.CalculateQuote().ToString(), quote.Desk.ProductionDays.ToString() };
-                dataGridView1.Rows.Add(row);
-
-                //This is just to make sure stuff is getting passed to the small text box. (just a check)
-                textBox1.Text = row[7];
-         
-                
-                
-            }
-
-            
+                dataGridView1.Rows.Add
+                (
+                    quote.FirstName,
+                    quote.LastName,
+                    quote.Desk.DesktopMaterial.ToString(),
+                    quote.Desk.Width.ToString(),
+                    quote.Desk.Depth.ToString(),
+                    quote.Desk.DrawerCount.ToString(),
+                    quote.CalculateQuote().ToString(),
+                    quote.Desk.ProductionDays.ToString()
+                );
+            });
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
